c3ea7c08f76fb77c0e529ad2c06f974d6ff64d83
abdulshakur <abdulshakur486@gmail.com>
2022-08-08 14:46:04 +0300

    customed.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,43 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\NetflixController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home');
+Route::post('newsletter', NewsletterController::class);
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [SessionsController::class, 'create'])->name('login');
+    Route::post('login', [SessionsController::class, 'store']);
+    Route::get('register', [RegisterController::class, 'create'])->name('register');
+    Route::post('register', [RegisterController::class, 'store']);
 });
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [SessionsController::class, 'destroy']);
+    Route::get('/movies', [NetflixController::class, 'index'])->name('netflix.index');
+    Route::get('/movie/{watch}', [NetflixController::class, 'show'])->name('movies.show');
+});
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('login/google', 'redirectToProvider');
+    Route::get('login/google/callback', 'handleProviderCallback');
+});
+
+
+// Route::get('ping', function () {
+//     $mailchimp = new MailchimpMarketing\ApiClient();
+//     $mailchimp->setConfig([
+//         'apiKey' => config('services.mailchimp.key'),
+//         'server' => 'us10',
+//     ]);
+
+//     $response = $mailchimp->lists->getAllLists();
+//     dd($response);
+// });
