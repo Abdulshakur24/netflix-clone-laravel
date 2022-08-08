2cd40642dbb69d7d829fe45e50c6f1583cea0f8a
abdulshakur <abdulshakur486@gmail.com>
2022-08-08 14:28:35 +0300

    completed.

--- app/Http/Controllers/NetflixController.php
+++ app/Http/Controllers/NetflixController.php
@@ -2,9 +2,76 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class NetflixController extends Controller
 {
-    //
+    public function index()
+    {
+        $fetcher = function ($slug, $type = "results") {
+            return Http::withToken(config('services.tmdb.token'))->get($slug)->json()[$type];
+        };
+
+        $populars = $fetcher('https://api.themoviedb.org/3/movie/popular');
+
+        $trending = $fetcher('https://api.themoviedb.org/3/trending/movie/day');
+
+        $netflixgenres = $fetcher('https://api.themoviedb.org/3/genre/movie/list', 'genres');
+
+        $comedies = $fetcher('https://api.themoviedb.org/3/discover/movie?api_key=MY_API_KEY&with_genres=35');
+
+        $action = $fetcher('https://api.themoviedb.org/3/discover/movie?api_key=MY_API_KEY&with_genres=28');
+
+        $western = $fetcher('https://api.themoviedb.org/3/discover/movie?api_key=MY_API_KEY&with_genres=37');
+
+        $horror = $fetcher('https://api.themoviedb.org/3/discover/movie?api_key=MY_API_KEY&with_genres=27');
+
+        $thriller = $fetcher('https://api.themoviedb.org/3/discover/movie?api_key=MY_API_KEY&with_genres=53');
+
+        $animation = $fetcher('https://api.themoviedb.org/3/discover/movie?api_key=MY_API_KEY&with_genres=16');
+
+        $genres = collect($netflixgenres)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
+
+        // dd([
+        //     'popular' => $populars,
+        //     'genres' => $genres,
+        //     'trending' => $trending,
+        //     'comedies' => $comedies,
+        //     'western' => $western,
+        //     'action' => $action,
+        //     'horror' => $horror,
+        //     'thriller' => $thriller,
+        //     'animation' => $animation,
+        // ]);
+
+        $index = array_rand($populars);
+
+        $popular = (object) $populars[$index];
+
+        return view('main', [
+            'popular' => $popular,
+            'populars' => $populars,
+            'genres' => $genres,
+            'trending' => $trending,
+            'comedies' => $comedies,
+            'western' => $western,
+            'action' => $action,
+            'horror' => $horror,
+            'thriller' => $thriller,
+            'animation' => $animation,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $playMovie = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/' . $id . '?append_to_response=credits,videos,images')
+            ->json();
+
+        return view('components.movies.show', [
+            'movies' => $playMovie,
+        ]);
+    }
 }
--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\Newsletter;
+use Exception;
+use Illuminate\Validation\ValidationException;
 
 class NewsletterController extends Controller
 {
-    //
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email' => 'required|email']);
+
+        try {
+            $newsletter->subscribe(request('email'));
+        } catch (Exception $e) {
+            throw ValidationException::withMessages([
+                'email' => 'This email could not be added to our newsletter.'
+            ]);
+        }
+
+        return redirect('/')->with('success', 'You are now signed up to our newsletter!');
+    }
 }
